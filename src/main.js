ca91998b646e53f7adc4ba5a37720ce0f411683e
zyz19 <80141182@qq.com>
2023-05-15 20:35:48 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import {getCategory} from "@/apis/testAPI";
+
+
+//测试接口函数
+getCategory().then(res =>{
+    console.log(res);
+})
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
