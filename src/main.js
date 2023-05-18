53f3af8ea0b2b7d0479c3c26a2403a41b9865573
zyz19 <80141182@qq.com>
2023-05-18 12:41:35 +0800

    优化全局指令

--- src/main.js
+++ src/main.js
@@ -6,31 +6,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'
-import {useIntersectionObserver} from "@vueuse/core";
+import {lazyPlugin} from "@/directives";
+
 
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
-//定义全局指令
-app.directive('img-lazy',{
-    mounted(el,binding){
-        //el : 指令绑定的哪个元素
-        //binding: binging.value 指令等于号后面绑定的表达式的值 图片url
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting}]) =>{
-                if( isIntersecting ){
-                    //图片进入视口区域，发送网络请求
-                    el.src = binding.value;
-                }
-            }
-        )
-    }
-})
+
 
 
